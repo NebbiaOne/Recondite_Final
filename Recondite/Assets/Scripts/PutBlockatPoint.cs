1f590fc90eed0c2fddf9b67581516790d16a0b86
NebbiaOne <fairhurstdylan42@gmail.com>
2018-05-27 00:14:36 +0200

    KeyCards, Raycast, Animator

---
+++ Recondite/Assets/Scripts/PutBlockatPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutBlockatPoint : MonoBehaviour {
+public GameObject obj;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		//if (Physics.Raycast (transform.position, Vector3.down, out hit))
+		if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+		{
+			if(hit.collider != GetComponent<Collider>() && hit.transform.tag != "Player")
+			obj.transform.position = hit.point;
+		}
+	}
+}
