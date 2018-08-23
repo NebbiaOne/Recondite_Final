23ebed5113a542cd7989a178ce21eb61a3d8f259
NebbiaOne <fairhurstdylan42@gmail.com>
2018-08-23 23:19:58 +0200

    Added Launch pad, recalculated key costs, PPP

---
+++ Recondite/Assets/Scripts/BounceConstant.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceConstant : MonoBehaviour {
+
+	[SerializeField] float vBounce = 700;
+	[SerializeField] float hBounce = 500;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other) {
+
+		other.rigidbody.AddForce (hBounce, vBounce, 0);
+			
+		}
+}
