1a539272856c6b4e0acec991513648cba2a6cf5e
NebbiaOne <fairhurstdylan42@gmail.com>
2018-09-13 22:57:51 +0200

    Lots of sounds

---
+++ Recondite/Assets/Scripts/SoundWPlayerCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundWPlayerCollision : MonoBehaviour {
+
+	// Use this for initialization
+	public AudioClip Clip;
+    AudioSource audioSource;
+	public GameObject source;
+	void Start () {
+		
+		audioSource = gameObject.GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+		
+	}
+		void OnCollisionEnter(Collision other) {
+
+			audioSource.PlayOneShot(Clip, 0.7F);
+		}
+}
